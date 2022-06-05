4b216f88124eece305beb6cc6310af97d1794c45
Deezor1 <skunks400@gmail.com>
2022-06-05 12:40:42 +0200

    fixes

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: 'imgix',
-    path: 'http://openweathermap.org/img/wn/',
+    path: 'https://openweathermap.org/img/wn/',
   },
 };
 
